327e8bb27cb775590c0bb15534b2d15478ca1795
Matias Novillo <novillo.matias1@gmail.com>
2024-02-17 06:22:40 -0300

    Se actualizo la connectionString de la app

--- FiyiStackApp/Models/Tools/connectionStrings.cs
+++ FiyiStackApp/Models/Tools/connectionStrings.cs
@@ -2,8 +2,8 @@
 {
     public static class connectionStrings
     {
-        public static string fiyistack_FiyiStackAppDevelopment = "data source =.; initial catalog = fiyistack_FiyiStackApp; Integrated Security = SSPI; MultipleActiveResultSets=True;Pooling=false;persist security info=True;App=EntityFramework";
+        public static string fiyistack_FiyiStackAppDevelopment = "data source =.; initial catalog = fiyistack_FiyiStackApp; Integrated Security = SSPI; MultipleActiveResultSets=True;Pooling=false;persist security info=True;App=EntityFramework;TrustServerCertificate=true";
 
-        public static string fiyistac_FiyiStackAppProduction = "data source =.; initial catalog = fiyistack_FiyiStackApp; Integrated Security = SSPI; MultipleActiveResultSets=True;Pooling=false;persist security info=True;App=EntityFramework";
+        public static string fiyistac_FiyiStackAppProduction = "data source =.; initial catalog = fiyistack_FiyiStackApp; Integrated Security = SSPI; MultipleActiveResultSets=True;Pooling=false;persist security info=True;App=EntityFramework;TrustServerCertificate=true";
     }
 }
